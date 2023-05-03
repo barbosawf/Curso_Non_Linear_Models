aca88639f5720bcc495b44407ab1dd2ab5b9a0b5
barbosawf <wagner.barbosa@ufv.br>
2023-05-03 02:18:52 -0300

    Almost finishing the general_plor_nls_list

--- general_plot_nls_list_01.R
+++ general_plot_nls_list_01.R
@@ -1,130 +1,256 @@
-dd <- data.frame(
-  predicted = rnorm(72, mean = 2, sd = 2),
-  state = rep(c("A", "B", "C"), each = 24)
-) 
 
-# Split the data by state, apply a function on each member that converts it into a 
-# plot object, and return the result as a vector.
-plots <- lapply(
-  split(dd, dd$state),
-  FUN = function(state_slice) {
-    # The code here is the plot code generation. You can do anything you would
-    # normally do for a single plot, such as calling stat_function, and you do this
-    # one slice at a time.
-    ggplot(state_slice, aes(predicted)) +
-      geom_density() +
-      stat_function(
-        fun = dnorm,
-        args = list(
-          mean = mean(state_slice$predicted),
-          sd = sd(state_slice$predicted)
-        ),
-        color = "red"
-      )
-  }
-)
+soy_nls = nls(weight ~ logistic(Time, Asym, xmid, scal),
+              start = list(Asym = 25, xmid = 60, scal = .103),
+              data = Soybean)
+soy_nls
 
 
-
-
-
-
-# Finally, present the plots on 3 columns.
-multiplot(plotlist = plots, cols=3)
-
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
+logistic = function(x, Asym, xmid, scal){
+  Asym / (1 + exp(- scal * (x - xmid)))
 }
 
+soylist = nlsList(weight ~ logistic(Time, Asym, xmid, scal),
+                  start = list(Asym = 18.4182, xmid = 53.9562, scal = 0.1226),
+                  data = Soybean)
+
+
+logistic_func <- function(x, .args) {
+  .args$Asym / (1 + exp(-.args$scal * (x - .args$xmid)))
+}
+
+fun <- logistic_func
+df <- Soybean
+nlsList.obj <- soylist
+x.axis <- 'Time'
+y.axis <- 'weight'
+grouping.var <- 'Plot'
+color = 'blue'
+linewidth = 0.5
+
+nlsList_plot <-
+  function(df,
+           x.axis,
+           y.axis,
+           fun,
+           nlsList.obj,
+           grouping.var,
+           color = 'blue',
+           linewidth = 1,
+           x.lab = NULL,
+           y.lab = NULL
+  ) {
+    
+    
+    # grouping.var is turned into symbol to be used in nest_by function
+    g.var <- sym(grouping.var)
+    
+    ordem <- sort(levels(df[[grouping.var]]))
+    
+    df |>
+      mutate_at(vars({{grouping.var}}), 
+                ~ factor(., levels = ordem)) |>
+      arrange_at(grouping.var) -> df
+    
+    
+    coef(nlsList.obj) |>
+      rownames_to_column(var = grouping.var) |>
+      mutate_at(vars({{grouping.var}}), 
+                ~ factor(., levels = ordem)) |>
+      arrange_at(grouping.var) -> arr.nlsList.obj
+    
+    
+    arr.nlsList.obj |>
+      na.omit() |>
+      nest_by({{g.var}}, .keep = T) -> nested_args
+    
+    
+    args_list <- nested_args$data
+    
+    
+    args_list |>
+      setNames(nested_args[[grouping.var]]) -> args_list
+    
+    
+    arr.nlsList.obj |>
+      apply(1, anyNA) -> id_with_nas
+    
+    arr.nlsList.obj |>
+      dplyr::select({{grouping.var}}) |> pull() -> gv
+    
+    id_with_nas <- as.vector(droplevels(gv[id_with_nas]))
+    
+    
+    x_min_max <- c(min = min(df[[x.axis]]), max = max(df[[x.axis]]))
+    
+    fit_values <- as.vector(na.omit(fitted(nlsList.obj)))
+    y_min_max <- c(min = min(c(fit_values, df[[y.axis]])),
+                   max = max(c(fit_values, df[[y.axis]])))
+    
+    
+    filtered_df <-
+      df |> filter_at(vars(all_of(grouping.var)), ~ !. %in% id_with_nas)
+    
+    
+    nested_df <- filtered_df |>
+      nest_by({{g.var}}, .keep = T)
+    
+    df_list <- nested_df$data |>
+      setNames(nested_df[[1]])
+    
+    df_list |>
+      map(~.x |>
+            summarise_at(vars({{x.axis}}), 
+                         list(min, max))) -> lms
+    
+    # x.axis and y.axis are turned into symbol to be used in geom_point function
+    x.axis <- sym(x.axis)
+    y.axis <- sym(y.axis)
+    
+    
+    
+    f.x <- function(x.lab) {
+      if (is.null(x.lab)) {
+        # A waiver is a "flag" object to indicate the calling function 
+        # should just use the default value.
+        xlab(waiver())
+      } else {
+        xlab(x.lab)
+      }
+    }
+    
+    f.y <- function(y.lab) {
+      if (is.null(y.lab)) {
+        ylab(waiver())
+      } else {
+        ylab(y.lab)
+      }
+    }
+    
+    
+    # the pmap function is needed to map objects into three lists simultaneously
+    # df_list contain a list of data separated by the grouping.var argument 
+    # therefore df_list is used in the geom_point function
+    # args_list contains the list of parameters estimated by the nlsList function
+    # therefore args_list is used in the stat_function function
+    # lms is a list with the limits of the x-axis where the non-linear function is applied
+
+    
+    pmap(list(df_list, args_list, lms), \(.df, .args, .lms) {
+      ggplot() +
+        geom_point(aes(x = {{x.axis}}, y = {{y.axis}}), data = .df) +
+        stat_function(
+          fun = \(x) fun(x, .args),
+          color = color,
+          xlim = c(.lms[[1]], .lms[[2]]),
+          linewidth = linewidth
+        ) +
+        scale_x_continuous(limits = x_min_max) +
+        scale_y_continuous(limits = y_min_max) +
+        labs(title = .args[[grouping.var]]) +
+        theme_minimal() +
+        theme(
+          plot.title = element_text(
+            size = 10,
+            hjust = 0.5,
+            vjust = -0.5
+          )
+        )  +
+        f.x(x.lab) +
+        f.y(y.lab)
+        
+    }) -> list_p
+    
+    
+    ggarrange(
+      plotlist = list_p |>
+        map( ~ . + rremove('xlab') +
+               rremove('ylab'))
+    ) |>
+      annotate_figure(
+        left = textGrob(
+          ifelse(is.null(y.lab), y.axis, y.lab),
+          rot = 90,
+          vjust = 0.5,
+          gp = gpar(cex = 1.2)
+        ),
+        bottom = textGrob(ifelse(is.null(x.lab), x.axis, x.lab), 
+                          gp = gpar(cex = 1.2))
+      ) -> panel_p
+    
+    return(list(list_p = list_p, panel_p = panel_p))
+    
+    
+  }
+
+
+nlsList_plot(df,
+             x.axis,
+             y.axis,
+             fun,
+             nlsList.obj,
+             grouping.var,
+             color = 'blue',
+             linewidth = 0.5,
+             x.lab = 'Time (days after planting)',
+             y.lab = 'Weight (g)'
+) -> pps
+
+
+pps$list_p[[1]]
+pps$panel_p
+
+# Usefull links -----------------------------------------------------------
+# https://stackoverflow.com/questions/1376967/using-stat-function-and-facet-wrap-together-in-ggplot2-in-r
+# https://stackoverflow.com/questions/64757410/shared-x-and-y-axis-labels-ggplot2-with-ggarrange
+
+
+# Garbage -----------------------------------------------------------------
 
 soylist |>
   map_dfr(\(x) if (!is.null(x))
     broom::tidy(x)[, 1:2], .id = 'ID') |>
   group_split(ID, .keep = T)
 
+x = length(p)
 
-tapply(warpbreaks$breaks, warpbreaks[,-1], sum)
+cols = round(sqrt(x),0)
+rows = ceiling(x/cols)
 
-
-v1 <- 'Plot'
-v2 <- sym(v1)
-coef(soylist) |>
-  na.omit() |> 
-  rownames_to_column(var = v1) |>
-  nest_by({{v2}}) -> nested_args
+ggarrange(plotlist = p |>
+            map(~ . + rremove('xlab') +
+                  rremove('ylab')), ncol=cols, nrow = rows)
 
 
-args_list <- nested_args$data
-
-args_list |>
-  setNames(coef(soylist) |>
-             na.omit() |>
-             rownames()) -> args_list
-
-coef(soylist) |> 
-  rownames_to_column(var = v1) |>
-  filter(is.na(Asym)) |>
-  dplyr::select({{v2}}) |> pull() -> id_with_nas
 
 
-filtered_df <-
-  Soybean |> filter_at(vars(all_of(v1)), ~ !. %in% id_with_nas)
+p
+cowplot::plot_grid(plotlist = p |>
+                     map(\(x) x + rremove('xlab') + rremove('ylab')),
+                   nrow = 5) |>
+  annotate_figure(
+    left = textGrob(
+      "Common y-axis",
+      rot = 90,
+      vjust = 0.5,
+      gp = gpar(cex = 1.3)
+    ),
+    bottom = textGrob("Common x-axis", gp = gpar(cex = 1.3))
+  )
 
 
-nested_df <- filtered_df |>
-  nest_by({{v2}})
 
-df_list <- nested_df$data |>
-  setNames(nested_df[[1]])
+require(ggpubr)
+ggarrange(plotlist = p |>
+            map(\(x) x + rremove('xlab') + rremove('ylab'))) |>
+  annotate_figure(
+    left = textGrob(
+      "Common y-axis",
+      rot = 90,
+      vjust = 1,
+      gp = gpar(cex = 1.3)
+    ),
+    bottom = textGrob("Common x-axis", gp = gpar(cex = 1.3))
+  )
 
-x <- sym('Time')
-y <- sym('weight')
-
-arguments <- c('Asym',  'xmid',  'scal')
-
-
-map2(df_list, args_list, \(.df, .args) {
-  ggplot() +
-    geom_point(aes(x = {{x}}, y = {{y}}), data = .df) +
-    geom_function(
-      fun = \(x) .args[[arguments[1]]] / (1 + exp(- .args[[arguments[3]]] * (x - .args[[arguments[2]]]))),
-      color = 'blue',
-      linewidth = 1
-    ) +
-    labs(x = x, y = y) +
-    theme_minimal()
-})
 
 
